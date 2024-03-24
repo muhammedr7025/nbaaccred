54cf5026896a6c3e39c149605494df48ef03c00b
Bijo Mathew Jose <bijomathewjose@gmail.com>
2024-03-24 13:59:10 +0530

    change import

--- src/pages/dashboard/department/bulkImport.ts
+++ src/pages/dashboard/department/bulkImport.ts
@@ -5,8 +5,6 @@ export async function bulkImportdepartments(data: any) {
         const departmentsData = data.map((item: any) => ({
             name: item.name,
             code: item.code,
-            mission_url: item.mission_url,
-            vision_url: item.vision_url,
         })
         )
         const newData = await supabase.from('departments').upsert(departmentsData, { ignoreDuplicates: true, onConflict: 'code' }).select('code')
