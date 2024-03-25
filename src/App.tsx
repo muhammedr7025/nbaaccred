811b34b8b9c486ed2a71077750375ecf0e98649d
muhammedr7025 <muhammedr7025@gmail.com>
2024-03-25 09:52:47 +0530

    web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import routes from "./routes"
 import { AuthProvider } from "./components/AuthContext"
-
+import { Analytics } from "@vercel/analytics/react"
 export default function App() {
   return (
     <AuthProvider>
       <RouterProvider router={createBrowserRouter(routes)} />
-
+      <Analytics/>
     </AuthProvider>
   )
 }
